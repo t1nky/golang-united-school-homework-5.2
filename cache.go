e466c098238c9b927b5600e699d928a69f9b673d
alex <haiiryy@gmail.com>
2022-07-14 17:38:45 +0400

    Solution

--- cache.go
+++ cache.go
@@ -1,23 +1,48 @@
 package cache
 
-import "time"
+import (
+	"time"
+)
+
+type Value struct {
+	expireAt time.Time
+	value    string
+}
 
 type Cache struct {
+	mem map[string]Value
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{
+		mem: make(map[string]Value),
+	}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c Cache) Get(key string) (string, bool) {
+	res, ok := c.mem[key]
+	if !res.expireAt.IsZero() && !time.Now().Before(res.expireAt) {
+		delete(c.mem, key)
+		return "", false
+	}
+	return res.value, ok
 }
 
-func (receiver) Put(key, value string) {
+func (c Cache) Put(key, value string) {
+	c.mem[key] = Value{expireAt: time.Time{}, value: value}
 }
 
-func (receiver) Keys() []string {
+func (c Cache) Keys() []string {
+	keys := make([]string, len(c.mem))
+	i := 0
+	for k := range c.mem {
+		keys[i] = k
+		i++
+	}
+
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c Cache) PutTill(key, value string, deadline time.Time) {
+	c.mem[key] = Value{expireAt: deadline, value: value}
 }
